a1323cd75a88a5d28fe91fafd604da352cc0a14e
Fredereick Anim Ansong <109765165+fred-ansong@users.noreply.github.com>
2025-11-27 23:58:22 -0600

    Update TriageOrder.java

--- TriageOrder.java
+++ TriageOrder.java
@@ -9,4 +9,18 @@ import java.util.Comparator;
 public final class TriageOrder implements Comparator<Patient> {
     // TODO: implement compare(...) WITHOUT reversing FIFO among ties
     // NOTE: Ensure the comparator is CONSISTENT WITH EQUALS expectations for PriorityQueue usage.
+
+    // Method to compare severity
+    @Override
+    public int compare(Patient a, Patient b) {
+
+        // Comparing severity to ensure higher severity comes first:
+        if (a.getSeverity() != b.getSeverity()) {
+            return Integer.compare(b.getSeverity(), a.getSeverity());
+            // since queues are in ascending order, we flip b with a to ensure higher severity gets treated first
+        }
+
+        // if severity is the same however, we compare arrivalseq (arrival order) and the lowest goes first
+        return Long.compare(a.getArrivalSeq(), b.getArrivalSeq());
+    }
 }
