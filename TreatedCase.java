f596687d524bf45245ee161d0545dfed99ab45d8
Fredereick Anim Ansong <109765165+fred-ansong@users.noreply.github.com>
2025-11-28 00:29:59 -0600

    Update TreatedCase.java

--- TreatedCase.java
+++ TreatedCase.java
@@ -1,3 +1,5 @@
+// Record of a patient case that has already been treated
+
 package edu.hcu.triage;
 
 import java.time.Instant;
@@ -13,4 +15,30 @@ public class TreatedCase {
     private final String notes;
 
     // TODO: constructor, getters, toString()
+    // Constructor initializing class fields
+    public TreatedCase(Patient patient, Instant start, Instant end, Outcome outcome, String notes) {
+        this.patient = patient;
+        this.start = start;
+        this.end = end;
+        this.outcome = outcome;
+        this.notes = notes;
+    }
+
+    // Getters for each of the class fields
+    public Patient getPatient() { return patient; }
+    public Instant getStart() { return Start; }
+    public Instant getEnd() { return end; }
+    public Outcome getOutcome() { return outcome; }
+    public String getNotes() { return notes; }
+
+    // toString method which determines what is printed when an instance of this class is printed
+    @Override
+    public String toString() {
+        return "TreatedCase:" +
+                "patient=" + patient +
+                ", start=" + start +
+                ", end=" + end +
+                ", outcome=" + outcome +
+                ", notes='" + notes + '\'';
+    }
 }
