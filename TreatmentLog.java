0a18de533fbf4eed7525c772fcd95825a84054ff
jlemessaa <jlemessaawsome@gmail.com>
2025-11-28 23:17:24 -0600

    update for treatmetlog

--- TreatmentLog.java
+++ TreatmentLog.java
@@ -1,5 +1,5 @@
 package edu.hcu.triage;
-
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,7 +8,27 @@ public class TreatmentLog {
     private final LinkedList<TreatedCase> log = new LinkedList<>();
 
     // TODO: append(TreatedCase tc)
+    public void append(TreadedCase tc) {
+        if (tc == null) {
+            throw new IllegalArgumentException("TreatedCase cannot be null");
+        }
+        log.addLast(tc);
+    }
+
     // TODO: size()
+    public int size() {
+        return log.size();
+    }
     // TODO: asListOldestFirst()
+    public List <TreatedCase> asListOldestFirst() {
+        return newArrayList<>(log);
+    }
+
+
     // TODO: asListNewestFirst()
+    public List<TreatedCase> asListNewestFirst() {
+        List<TreatedCase> copy = new ArrayList<>(log);
+        Collections.reverse(copy);
+        return copy;
+    }
 }
